113f3aad3a826094643e548f38f17dec0a7f6470
AhmedAfzal2089 <126315364+AhmedAfzal2089@users.noreply.github.com>
2025-03-17 12:07:11 +0500

    pf lab 1 tasks

---
+++ PF Lab 1/task2.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int marks ;
+//	cout << "Enter Your marks: ";
+//	cin >> marks;
+//	if (marks >= 90 && marks <= 100) {
+//		cout << "Grade = A";
+//	}
+//	else if (marks >= 80 && marks <= 89) {
+//		cout << "Grade = B";
+//	}
+//	else if (marks >= 70 && marks <= 79) {
+//		cout << "Grade = C";
+//	}
+//	else if (marks >= 60 && marks <= 69) {
+//		cout << "Grade = D";
+//	}
+//	else if (marks < 60 ) {
+//		cout << "Grade = F";
+//	}
+//	return 0;
+//}
