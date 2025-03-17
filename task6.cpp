d716ea73b849a329adf41d584ba34027055a59f3
AhmedAfzal2089 <126315364+AhmedAfzal2089@users.noreply.github.com>
2025-03-17 12:00:11 +0500

    pf lab 1 tasks

---
+++ task6.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int num;
+//	cout << "Enter a numvber: ";
+//	cin >> num;
+//	int stars = 1;
+//	int spaces = num - 1;
+//	for (int i = 0; i < num; i++) {
+//		for (int j = 0; j < spaces; j++) {
+//			cout << " ";
+//		}
+//		for (int k = 0; k < stars; k++) {
+//			cout << "* ";
+//		}
+//		cout << endl;
+//		stars++;
+//		spaces--;
+//	}
+//	spaces = 0;
+//	stars = num;
+//	for (int i = 0; i < num; i++) {
+//		stars--;
+//		spaces++;
+//		for (int j = 0; j < spaces; j++) {
+//			cout << " ";
+//		}
+//		for (int k = 0; k < stars; k++) {
+//			cout << "* ";
+//		}
+//		cout << endl;
+//	}
+//}
